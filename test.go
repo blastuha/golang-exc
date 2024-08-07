b00a26106f9ceb3719a86acd5ba93e3dae8c99a7
Boris <shevnin.boris2@yandex.ru>
2024-08-07 22:03:57 +0300

    Новые файлы

--- test.go
+++ test.go
@@ -18,39 +18,56 @@ func getValuesFromInput() (string, string, string) {
 
 	line, _ := reader.ReadString('\n')
 	noSpaceLine := strings.ReplaceAll(line, " ", "")
+	clearLine := strings.TrimSpace(noSpaceLine)
 
 	firstValue := ""
 	secondValue := ""
 	operator := ""
 
-	for index, value := range noSpaceLine {
+	for index, value := range clearLine {
 		if slices.Contains(operatorSlice, string(value)) {
-			firstValue = noSpaceLine[:index]
-			secondValue = noSpaceLine[index+1:]
-			operator = string(noSpaceLine[index])
+			firstValue = clearLine[:index]
+			secondValue = clearLine[index+1:]
+			operator = string(clearLine[index])
 		}
-	}
-
-	_, err := strconv.Atoi(firstValue)
-	if err == nil {
-		panic("First value is a number")
-	}
-
-	if len(firstValue) > 10 || len(secondValue) > 10 {
-		panic("Длина одного из значений > 10")
 	}
 
 	return firstValue, secondValue, operator
 }
 
-func calculate(firstValue, secondValue, operator string) {
-	switch operator {
-	case "+":
-
+func calculate(firstValue, secondValue, operator string) string {
+	// проверка firstValue на число
+	_, err1 := strconv.Atoi(firstValue)
+	if err1 == nil {
+		panic("First value is a number")
 	}
+	// перевод secondValue в число и проверка числа на  > 10
+	secondNumber, err2 := strconv.Atoi(secondValue)
+	if err2 != nil {
+		fmt.Println("Второе значение - это строка", err2)
+	}
+	if secondNumber > 10 {
+		panic("Second value as number > 10")
+	}
+	// проверка values на длину строки > 10
+	if len(firstValue) > 10 || len(secondValue) > 10 {
+		panic("Длина одного из значений > 10")
+	}
+
+	// сложение со строкой или числом
+	if operator == "+" {
+		return firstValue + secondValue
+	}
+	// сложение с числом
+	if operator == "-" {
+		return firstValue + secondValue
+	}
+
+	return ""
 
 }
 
 func main() {
-	getValuesFromInput()
+	//fmt.Println(getValuesFromInput())
+	fmt.Println(calculate(getValuesFromInput()))
 }
